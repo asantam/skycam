71441a74ec438f6d9bfe2ba51a2d24a5130f824f
Andres Santamaria Artigas <asantam@umd.edu>
2020-01-24 11:46:10 -0500

    Delete aux folder

--- scripts/aux/l3_tunnel_keep.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-while :; do
-	ps aux | grep [l]tdri3 &>/dev/null && echo "Connection already present" || ssh -f -i /home/control/.ssh/id_rsa -R 19996:localhost:22 andres@ltdri3.geog.umd.edu sleep 31536000
-sleep 1m
-done
-
-exit 0
