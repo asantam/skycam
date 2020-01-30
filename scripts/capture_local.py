af777094248a4bf50cd88a5482ea8b40ad2257a8
Andres Santamaria Artigas <asantam@umd.edu>
2020-01-30 12:03:11 -0500

    Began using black code style

--- scripts/capture_local.py
+++ scripts/capture_local.py
@@ -15,14 +15,14 @@ from picamera import PiCamera
 
 def capture_images(
     output_basename,
-    image_format='jpeg',
+    image_format="jpeg",
     ev_list=[0.3, 0.8, 1.0, 1.2],
     width=2492,
     height=1944,
     iso=100,
     quality=100,
     bayer=True,
-    meter_mode='average',
+    meter_mode="average",
 ):
     """
     Main capture routine, passes most arguments to picamera
@@ -33,10 +33,7 @@ def capture_images(
     Returns a list of the captured images paths.
     """
     # Initialize camera
-    with PiCamera(
-        resolution=(width, height),
-        framerate=5,
-    ) as camera:
+    with PiCamera(resolution=(width, height), framerate=5,) as camera:
         # Configure meter_mode and iso
         camera.meter_mode = meter_mode
         camera.iso = iso
@@ -45,10 +42,10 @@ def capture_images(
         while camera.exposure_speed == 0:
             time.sleep(0.1)
         # Now stop automatic exposure and white balance, and fix gains
-        camera.exposure_mode = 'off'
+        camera.exposure_mode = "off"
         exposure_speeds = [int(camera.exposure_speed * e) for e in ev_list]
         awb_gains = camera.awb_gains
-        camera.awb_mode = 'off'
+        camera.awb_mode = "off"
         camera.awb_gains = awb_gains
         # Create a list of streams to capture the images.
         # To reduce the time between captures, each image is compressed
@@ -66,14 +63,14 @@ def capture_images(
     output_file_list = []
     for i, ev in enumerate(ev_list):
         output_file_list.append(
-            '{}_{}.{}.bz2'.format(
+            "{}_{}.{}.bz2".format(
                 output_basename,
-                str(ev_list[i]).replace('.', 'p'),
+                str(ev_list[i]).replace(".", "p"),
                 image_format,
             )
         )
         stream_list[i].seek(0)
-        with open(output_file_list[i], 'wb') as f:
+        with open(output_file_list[i], "wb") as f:
             f.write(
                 # stream_list[i].read()
                 bz2.compress(stream_list[i].read())
@@ -82,26 +79,25 @@ def capture_images(
     return output_file_list
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Define output file options
-    dir_tmp = '/dev/shm/'
-    dir_out = '/home/data/data/'
+    dir_tmp = "/dev/shm/"
+    dir_out = "/home/data/data/"
     # Get information from system to name output files
     hostname = socket.gethostname()
     gmt = time.gmtime()
-    timestamp = time.strftime('%Y%m%dT%H%M%S', gmt)
-    date = time.strftime('%Y%m%d', gmt)
-    basename = hostname + '_' + timestamp
+    timestamp = time.strftime("%Y%m%dT%H%M%S", gmt)
+    date = time.strftime("%Y%m%d", gmt)
+    basename = hostname + "_" + timestamp
 
     # Run capture routine
     files_out = capture_images(dir_tmp + basename)
     # Archive files into daily tar and remove them from ramdisk
-    file_tar = dir_out + hostname + '_' + date + '.tar'
-    with tarfile.open(file_tar, 'a') as tar:
+    file_tar = dir_out + hostname + "_" + date + ".tar"
+    with tarfile.open(file_tar, "a") as tar:
         for file_name in files_out:
             tar.add(
-                file_name,
-                arcname=file_name.replace(dir_tmp, basename + '/'),
+                file_name, arcname=file_name.replace(dir_tmp, basename + "/"),
             )
             os.remove(file_name)
     print(file_tar)
