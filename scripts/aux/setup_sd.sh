a83a2a7e6236bde5033f8a3b761c3122f727e2b5
Andres Santamaria Artigas <andres.santamaria.artigas@nasa.gov>
2020-03-02 20:59:33 +0000

    Add script to create data partition

---
+++ scripts/aux/setup_sd.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Check for root
+if test "$(id -u)" -ne "0"; then
+  printf "%s\n" "Must be run as root" >&2
+  exit 1
+fi
+
+# Generate new partition for data
+printf "n\np\n3\n3612672\n\nw" | fdisk /dev/mmcblk0
+
+# Format new partition
+mkfs.ext4 /dev/mmcblk0p3
+
+# Add entry to fstab
+>>/etc/fstab printf "%s\n" "/dev/mmcblk0p3 /home/control/data ext4 defaults,noatime,nofail,x-systemd.device-timeout=9 0 2"
+
+# Mount and change ownership
+mount /dev/mmcblk0p3
+chown control:control /home/control/data
